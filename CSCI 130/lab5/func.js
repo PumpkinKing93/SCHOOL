868bf448afaa98dc685621bf8cbd7ee851b45a47
PumpkinKing93 <93gymnast@gmail.com>
2019-09-27 15:58:25 -0700

    130 Lab calculator

--- CSCI 130/lab5/func.js
+++ CSCI 130/lab5/func.js
@@ -14,3 +14,17 @@ function solve() {
 function clr() {
     document.getElementById("result").value = ""
 } 
+
+// sqrt a number
+function sqrt(){
+    let x = document.getElementById("result").value
+    let y = Math.sqrt(x)
+    document.getElementById("result").value = y
+}
+
+//function that display value 
+function neg() {
+    let x = document.getElementById("result").value
+    let y = eval(-1*x)
+    document.getElementById("result").value = y
+}
