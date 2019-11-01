38e197b24d5462ba2aa54f689691a7cfdc302640
PumpkinKing93 <93gymnast@gmail.com>
2019-11-01 12:17:01 -0700

    working on Lab 10

--- CSCI 130/Labs/lab10/functions.php
+++ CSCI 130/Labs/lab10/functions.php
@@ -1,4 +1,35 @@
 <?php
 
+class Student {
+    
+    private $firstName; 
+    private $lastName; 
+    private $address;
+    private $born;
+    private $idNum; 
+    private $gpa; 
+    private $numberOfUnits;
+ 
+	function __construct( $name, $age ) {
+		$this->firstName = $firstName;
+        $this->lastName = $lastName;
+        $this->address = $address;
+        $this->born = $born;
+        $this->idNum = $idNum;
+		$this->gpa = $gpa;
+		$this->numberOfUnits = $numberOfUnits;
+        
+        
+	}
+ 
+	function getName() {
+		return $this->name;
+	}
+ 
+	function isAdult() {
+		return $this->age >= 18?"an Adult":"Not an Adult";
+	}
+ 
+}
 
 ?>
